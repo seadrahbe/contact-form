9a7cfdab79f57b0f2a9fda3cb5ee5394145813aa
seadrahbe <174991882+seadrahbe@users.noreply.github.com>
2026-02-15 15:42:59 -0800

    Updating port num

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ import express from 'express';
 const app = express();
 
 // Define the port number where our server will listen
-const PORT = 3000;
+const PORT = 3002;
 
 app.use(express.static('public'));
 
