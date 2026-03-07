ecdf6fe0095e58c0b72009d27a5485d0f666b3c6
seadrahbe <174991882+seadrahbe@users.noreply.github.com>
2026-03-06 17:41:28 -0800

    Major design overhaul, combined w HTML resume, added nav partial, added admin page functionality and confirm page functionality with EJS, updated CSS for all pages.

--- app.js
+++ app.js
@@ -1,37 +1,47 @@
 // Import the express module
 import express from 'express';
 
-// Create an instance of an Express application
-const app = express();
-
 // Define the port number where our server will listen
 const PORT = 3002;
+
+// Create an instance of an Express application
+const app = express();
 
 app.use(express.static('public'));
 
 // "Middleware" allows express to read form data and store it in req.body
 app.use(express.urlencoded({ extended: true }));
 
-// Create a temp array to store orders -- const makes pointer / reference stay, list can grow/shrink
+// Set view engine to ejs
+app.set('view engine', 'ejs');
+
+// Create a temp array to store contacts -- const makes pointer / reference stay, list can grow/shrink
 const contacts = [];
 
 // Define a default "route" ('/')
 // req: contains information about the incoming request
 // res: allows us to send back a response to the client
+
 app.get('/', (req, res) => {
 
-   res.sendFile(`${import.meta.dirname}/views/home.html`);
+   res.render("home");
+
+});
+
+app.get('/contact', (req, res) => {
+
+   res.render("contact");
 
 });
 
 //  Confirmation
 app.get('/thank-you', (req, res) => {
-    res.sendFile(`${import.meta.dirname}/views/confirmation.html`);
-    // Test
+
+    res.render("confirmation", { contact });
+
 });
 
 // Submit contact route
-//
 app.post('/submit-contact', (req, res) => {
 
   // Create a JSON object to store the order data
@@ -40,6 +50,8 @@ app.post('/submit-contact', (req, res) => {
     lname: req.body.lname,
     title: req.body.jtitle,
     company: req.body.company,
+    linkedin: req.body.linkedin,
+    email: req.body.email,
     met: req.body.meet,
     other: req.body.other,
     message: req.body.message,
@@ -48,16 +60,17 @@ app.post('/submit-contact', (req, res) => {
     timestamp: new Date()
   };
 
-  // Add order object to orders array
+  // Add object to array
   contacts.push(contact);
 
   // Send user to thank you page
-  res.sendFile(`${import.meta.dirname}/views/confirmation.html`);
+  res.render("confirmation", { contact });
+
 });
 
 // Admin route
 app.get('/admin', (req, res) => {
-    res.send(contacts);
+    res.render("admin", { contacts });
 });
 
 
